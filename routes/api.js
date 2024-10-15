fad5ff7647c5c2bf89950ecdba9e83bd12cd9e8a
bmcgauley <brian@mcgauley.com>
2024-10-15 09:38:22 -0700

    Revert "Lot of work done here, caught back up and made some progress"
    This reverts commit f9ebf176792ad321869cf48c0a6fda6b3967aca8.

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pokemonController = require('../controllers/pokemonController');
-
-router.get('/pokemon', pokemonController.getAllPokemon);
-router.get('/pokemon/:id', pokemonController.getPokemonById);
-
-module.exports = router;
